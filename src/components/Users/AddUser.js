91f7ad22be2be79cd63fe1265348463396ffb4c1
jeton ramadani <jeton.ramadani@students.finki.ukim.mk>
2021-06-18 00:16:49 +0200

    Adding Card component a wrapper component and styling the form

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,21 @@
+import styles from "./AddUser.module.css";
+import Card from "../UI/Card";
+const AddUser = (props) => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" />
+        <label htmlFor="age">Age (Years)</label>
+        <input type="number" id="age" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
