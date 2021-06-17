ed7fe653b4712c1a44590d0eefedbbc33ca8181a
jeton ramadani <jeton.ramadani@students.finki.ukim.mk>
2021-06-18 00:25:18 +0200

    Custom Button

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,5 +1,7 @@
 import styles from "./AddUser.module.css";
 import Card from "../UI/Card";
+import Button from "../UI/Button";
+
 const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
@@ -12,7 +14,7 @@ const AddUser = (props) => {
         <input type="text" id="username" />
         <label htmlFor="age">Age (Years)</label>
         <input type="number" id="age" />
-        <button type="submit">Add User</button>
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
